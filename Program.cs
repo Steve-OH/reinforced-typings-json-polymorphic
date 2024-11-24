1e50ca5ec09262b08b476c44ce19e156c3e86477
Steve Schafer <roller@donacobi.us>
2024-11-23 20:46:15 -0500

    Initial and only commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace PolymorphicTest;
+
+using System.Text.Json.Serialization;
+
+[JsonPolymorphic(TypeDiscriminatorPropertyName = "Name")]
+internal record Foo(
+    [property: JsonPropertyName("name")]
+    string Name);
+
+internal class Program
+{
+    static void Main()
+    {
+    }
+}
